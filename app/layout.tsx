cb68d2e3b93d8880ca14f5d5006e766e500278c4
ahmadelraqab <amjrr99@gmail.com>
2023-11-30 17:41:08 +0300

    idk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './globals.scss'
 import Navbar from '@/components/navbar'
 import Image from 'next/image'
 import Logos from '@/components/logos'
